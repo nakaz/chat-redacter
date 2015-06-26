5a32c398a6141c55663828870edff6a10aaac213
Sean Nakamura <seanenakamura@gmail.com>
2015-06-25 20:22:14 -1000

    working js and index nao

---
+++ filter.js
@@ -0,0 +1,7 @@
+module.exports = {
+  log: log
+};
+
+function log (){
+  console.log('hello');
+}
--- server.js
+++ server.js
@@ -4,10 +4,17 @@ var app = express();
 var jade = require('jade');
 var bodyParser = require('body-parser');
 
+var modules = require('./filter.js');
+
 app.set('views', './');
 app.set('view engine', 'jade');
 
 app.set('title', 'Redacter');
+
+app.use ('/', function(req, res, next){
+  modules.log();
+  next();
+});
 
 app.get('/', function(req, res){
   res.render('index', {title: 'hello!', message: 'what the hell?!'});
